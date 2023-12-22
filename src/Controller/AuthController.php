5fe3aead6a337e49f5ab768238e1b24fa56cecf0
Retch <retch100@gmail.com>
2023-12-22 23:20:05 +0100

    add functionality to register user

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,5 +16,37 @@ class AuthController extends AbstractController {
     public function checkJwt() : Response
     {
         return new Response('JWt is valid', 200);
+    }
+
+    #[Route('/api/register', name: 'app_api_register_user', methods: ['POST'])]
+    public function registerUser(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, Request $request): Response
+    {
+        $json = json_decode($request->getContent(), true);
+
+        if (!isset($json["username"]) || !isset($json["password"])) {
+            return new Response('Username or password not set', 400);
+        }
+
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $json["username"]]);
+        if ($existingUser) {
+            return new Response('User already exists', 400);
+        }
+
+        $user = new User();
+        $user->setEmail($json["username"]);
+        $user->setRoles(['ROLE_USER']);
+        $plaintextPassword = $json['password'];
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response('User created', 200);
+
     }
 }
